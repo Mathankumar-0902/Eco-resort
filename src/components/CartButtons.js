22b806ceb0823eb847186ee4186bf395cad510cf
Mathankumar-0902 <126561381+Mathankumar-0902@users.noreply.github.com>
2023-08-19 02:11:15 +0530

    Update CartButtons.js

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaUserMinus, FaUserPlus } from "react-icons/fa";
 import styled from "styled-components";
-
 import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
@@ -29,44 +28,8 @@ const CartButtons = () => {
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-  width: 225px;
+  width: 110px;
 
-  .cart-btn {
-    color: var(--clr-grey-1);
-    font-size: 1.5rem;
-    letter-spacing: var(--spacing);
-    color: var(--clr-grey-1);
-    display: flex;
-
-    align-items: center;
-  }
-  .cart-container {
-    display: flex;
-    align-items: center;
-    position: relative;
-    svg {
-      height: 1.6rem;
-      margin-left: 5px;
-    }
-  }
-  .cart-value {
-    position: absolute;
-    top: -10px;
-    right: -16px;
-    background: var(--clr-primary-5);
-    width: 16px;
-    height: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    font-size: 0.75rem;
-    color: var(--clr-white);
-    padding: 12px;
-  }
   .auth-btn {
     display: flex;
     align-items: center;
